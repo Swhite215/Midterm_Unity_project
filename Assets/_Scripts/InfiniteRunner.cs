8ea8ee3dcdaf5b602fd063c49e16fa5d44f58dc1
Spencer White <Spencer.White@carat.com>
2018-02-17 12:00:35 -0500

    Added simple procedural generation

---
+++ Assets/_Scripts/InfiniteRunner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteRunner : MonoBehaviour {
+
+	public GameObject spawnPoint;
+	public GameObject[] roads;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {
+			//Pick a random number
+			//Instantiate the prefab at the spawnPoint from the prefabs array.
+
+			int randomSpawnPoint = (int)Random.Range (0, roads.Length);
+			Debug.Log (randomSpawnPoint);
+
+			Object.Instantiate (roads [randomSpawnPoint], spawnPoint.transform.position, roads[randomSpawnPoint].transform.rotation);
+
+		}
+	}
+}
